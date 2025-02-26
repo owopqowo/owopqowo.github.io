7c779e65a457a7631febe46bf7f89dcae0ceb203
owopqowo <owo9370@gmail.com>
2025-02-26 17:20:57 +0900

    feat: 컨텐츠 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 function App() {
   return (
     <>
-      <div>intro</div>
-      <div>about</div>
-      <div>work</div>
-      <div>contact</div>
+      <div>
+        <h1>안녕하세요. UI 개발자 원인정입니다.</h1>
+        <p>
+          재사용성이 높으며 깔끔한 코드를 작성하기 위해 노력합니다.
+          <br />
+          인터렉션 구현에 관심을 가지고 공부하고 있습니다.
+        </p>
+      </div>
+      <div>
+        <div>About</div>
+        <div>
+          웹 표준, 웹 접근성에 맞춰 작업하며 의미에 맞는 태그를 사용합니다. 또한 간결하고 모듈화된 코드를 지향합니다.
+        </div>
+        <div>기획자, 디자이너, 개발자와 함께 소통하며 프로젝트 완성을 위해 노력합니다.</div>
+        <div>최신 트렌드를 놓치지 않기 위해 검색하고 공부하며, 부족한 부분을 채워나가고 있습니다.</div>
+      </div>
+      <div>
+        <div>Work</div>
+      </div>
+      <div>
+        <div>Contact</div>
+        owo9370@gmail.com
+        <a href="mailto:owo9370@gmail.com" target="_blank">
+          메일 보내기
+        </a>
+      </div>
     </>
   );
 }
