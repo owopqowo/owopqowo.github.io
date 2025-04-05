6bbb5c4086c008175fd9df3f126a00f107ae6573
owopqowo <owo9370@gmail.com>
2025-04-05 16:42:37 +0900

    fix: footer padding 값 수정

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-stone-800 px-4 py-[20vh] text-center">
+    <footer className="bg-stone-800 px-4 py-24 text-center">
       <div className="mb-8">
         <div className="relative mx-auto h-6 w-72 overflow-hidden text-right">
           <span className="absolute top-0 right-0 animate-[across_6s_linear_infinite] text-sm text-neutral-100 opacity-0">
